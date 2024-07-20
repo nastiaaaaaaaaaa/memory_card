3d4e6a56a5f9c7c2a6fb8cf2a919360fc1128d39
nastiaaaaaaaaaa <116444052+nastiaaaaaaaaaa@users.noreply.github.com>
2024-07-20 16:52:51 +0300

    Update main.py

--- memory_card/main.py
+++ memory_card/main.py
@@ -1,41 +1,45 @@
-from main_window import *
 from menu_window import *
+from main_window import *
 
-from random import choice, shuffle
+from random import choice, shuffle #виб. ранд. ел.зі списку \ перемішує елементи списку
 from time import sleep
 
-wins = [main_wind, menu_wind]
+wins = [main_win, menu_win]
 for win in wins:
     win.setStyleSheet('''
-                      color:yellow
-                      backround-color:lightblue
-                      font-size: 20xp;
-                      border: 0.5px solid blue:
+                      color: #18d4f5;   
+                      background-color: #000000;
+                      font-size: 20px;
+                      border: 0.5px solid blue;
                       ''')
-
+    
 lb_Result.setStyleSheet('margin: 20px;')
 
-app = QApplication([])
+# колір тексту
+# колір фону
+# розмір тексту
+# розмір і колір рамок
 
+# клас Питання 
 class Question():
-    def __init__(self,question,answer,wrong_answer1,wrong_answer2,wrong_answer3): # конструктор
-        self.question = question # питання
-        self.answer = answer # відповідь
-        self.wrong_answer1 = wrong_answer1 # неправильні відповіді 1 - 3
+    def __init__(self, question, answer, wrong_answer1, wrong_answer2, wrong_answer3):
+        self.question = question             # питання
+        self.answer = answer                 # відповідь
+        self.wrong_answer1 = wrong_answer1   # непр.відп.1
         self.wrong_answer2 = wrong_answer2
         self.wrong_answer3 = wrong_answer3
-        
-        self.actual = True # чи актуальне питання
-        self.attempts = 0 # спроби
-        self.correct = 0 # правильно чи ні
+
+        self.actual = True  # чи актуальне питання
+        self.attempts = 0   # кільк. спроб
+        self.correct = 0    # кільк. прав.відп.
 
     def got_right(self):
-        # змінює статистику, отримавши правильну відповідь
+        ''' змінює статистику, отримавши правильну відповідь'''
         self.attempts += 1
         self.correct += 1
-    
+
     def got_wrong(self):
-        # змінює статистику, отримавши неправильну відповідь
+        ''' змінює статистику, отримавши неправильну відповідь'''
         self.attempts += 1
 
 # питання
@@ -48,90 +52,111 @@ q6 = Question('Перший президент України', 'Леонід К
 q7 = Question('Найвища гора?', 'Еверест', 'Говерла', 'Лхоцзе', 'Чогорі')
 
 
-# списки з кнопками та питаннями
-radio_list = [rbtn1, rbtn2, rbtn3, rbtn4] # кнопки
-questions = [q1, q2, q3, q4, q5, q6, q6, q7] # питання
-# задаємо питання
+# список з перемикачів кнопок та питань
+radio_list = [rbtn_1, rbtn_2, rbtn_3, rbtn_4]
+questions = [q1, q2, q3, q4]
 
+#                                         (функція що обирає випадкове запитання зі списку та показує його на екрані)
 def new_question():
-    global current_question
-    current_question = choice(questions) # питання
+    global cur_question
+    cur_question = choice(questions)                   #вибирає рандомне запитання
 
-    lb_Question.setText(current_question.question) # беремо питання та правильну відповідь
-    lb_Correct.setText(current_question.answer)
+    lb_Question.setText(cur_question.question)         #встановлює текст(відповіді та запитання) на віджети
+    lb_Correct.setText(cur_question.answer)
 
-    shuffle(radio_list) # перемішуємо кнопки
+    shuffle(radio_list)                                 #перемішує кнопки(щоб не на 1 шій була завжди прав.відпов.)- змінюючи позицію в списку
+    
+    radio_list[0].setText(cur_question.wrong_answer1)   #розмішюємо на них знову питання
+    radio_list[1].setText(cur_question.wrong_answer2)
+    radio_list[2].setText(cur_question.wrong_answer3)   #в новому порядку
+    radio_list[3].setText(cur_question.answer)
 
-    radio_list[0].setText(current_question.wrong_answer1) # задаємо кнопкам варіанти відповідей
-    radio_list[1].setText(current_question.wrong_answer2)
-    radio_list[2].setText(current_question.wrong_answer3)
-    radio_list[3].setText(current_question.answer)
-new_question()
-# перевірка відповіді
+
+#                                                   (функція для перевірки результату відповіді)
 def check_result():
-    for ans_btn in radio_list: # отримуємо кнопки з списка
-        if ans_btn.isChecked(): # якщо кнопку нажали
-            if ans_btn.text() == lb_Correct.text(): # перевіряємо якщо текст кнопки правильний
-                current_question.got_right()
-                lb_Result.setText('Правильно') # правильно
+    for ans_btn in radio_list:
+        if ans_btn.isChecked():                         # вибраний вірних перемикач?
+            if ans_btn.text() == lb_Correct.text():     # чи збігається текст на вибр.кпонці та текст прав.відповіді?
+                cur_question.got_right()                # збільшити кільк.спроб +1
+                lb_Result.setText('Правильно!')
+                update_statistics()        
                 break
     else:
-        current_question.got_wrong()
-        lb_Result.setText('Неправильно') # неправильно
-# відпочити
-def rest(): 
-    main_wind.hide() # прячемо головне вікно
-    n = box_Minutes.value() # беремо встановлене число
-    sleep(n) # відпочинок
-    main_wind.show() # показуємо головне вікно
+        cur_question.got_wrong()                 #якщо не вибр.прав.відп.
+        lb_Result.setText('Неправильно! :)')     #змінити надпис на НЕПРАВИЛЬНО
+        update_statistics()
 
-def show_menu(): # показати меню
-    menu_wind.show() # показуємо меню
-    main_wind.hide() # ховаємо головне вікно
+#                                               (функцію-обробник кнопки “Відпочити”)
+def rest():
+    main_win.hide()                 # приховати головний віджет    
+    n = box_Minutes.value() * 60    # бере знач.від корситвача і множить на кільк. секунд
+    sleep(n)                        # спить
+    main_win.show()                 # потім знову показує
 
-def back_menu(): # показати головне вікно
-    menu_wind.hide() # прячемо меню
-    main_wind.show() # показуємо головне вікно
+# ----------------------------------------------------------
+#  Функції для кнопок панелі - дод.питання та статистика
+# ----------------------------------------------------------
+def show_menu():      
+    menu_win.show()
+    main_win.hide()
 
-def clear(): 
-    # очищуємо питання
+def back_menu():
+    menu_win.hide()
+    main_win.show()
+
+def clear():
     txt_Question.clear()
     txt_Answer.clear()
     txt_Wrong1.clear()
     txt_Wrong2.clear()
     txt_Wrong3.clear()
 
-def addQuestion(): # додаємо питання
-    newq = Question(txt_Question.text(), txt_Answer.text(), txt_Wrong1.text(), txt_Wrong2.text(), txt_Wrong3.text()) # задаємо варіанти відповіді
-    questions.append(newq) # додаємо питання в список
-    clear() # очищуємо
+def add_question():
+    newq = Question(txt_Question.text(), txt_Answer.text(), txt_Wrong1.text(), txt_Wrong2.text(), txt_Wrong3.text())
+    questions.append(newq)
+    clear()
 
+#   функція зміни(запинать та надписів на кнопках)
 def switch_screen():
     if btn_OK.text() == 'Відповісти':
-        check_result()
+        check_result()   #
         RadioGroupBox.hide()
         AnsGroupBox.show()
-        btn_OK.setText('Наступне питання')
+        btn_OK.setText('Наступне запитання')
     else:
-        new_question()
+        new_question() #
         RadioGroupBox.show()
         AnsGroupBox.hide()
         btn_OK.setText('Відповісти')
+                                            # скинути вибрану радіо-кнопку
+        RadioGroup.setExclusive(False) # зняли обмеження, щоб можна було скинути вибір радіокнопки
+        rbtn_1.setChecked(False)
+        rbtn_2.setChecked(False)
+        rbtn_3.setChecked(False)
+        rbtn_4.setChecked(False)
+        RadioGroup.setExclusive(True) # повернули обмеження, тепер лише одна радіокнопка може бути вибрана
 
-        RadioGroup.setExclusive(False)
-        rbtn1.setChecked(False)
-        rbtn2.setChecked(False)
-        rbtn3.setChecked(False)
-        rbtn4.setChecked(False)
+# ----------------------------------------------------------
+# Функція для оновлення статистики і вкиликати в перевірці
+# ----------------------------------------------------------
+def update_statistics():
+    total_attempts = sum(q.attempts for q in questions)
+    total_correct = sum(q.correct for q in questions)
+    lbl_statistics.setText(f"Статистика:\nПравильних відповідей: {total_correct}\nЗагальна кількість спроб: {total_attempts}")
 
-# Підключення виклику функції до кнопок
+# ----------------------------------------------------------
+#  Пікючення виклику функції - до кнопок
+# ----------------------------------------------------------
+
+new_question()
 
 btn_Menu.clicked.connect(show_menu)
 btn_back.clicked.connect(back_menu)
 btn_clear.clicked.connect(clear)
 btn_Sleep.clicked.connect(rest)
 btn_OK.clicked.connect(switch_screen)
-btn_add_q.clicked.connect(addQuestion)
+btn_add_q.clicked.connect(add_question)
 
-main_wind.show()
+main_win.show()
 app.exec_()
+
