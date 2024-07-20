6d8cd047527685e68f3e8e25600ab285f1839d94
nastiaaaaaaaaaa <116444052+nastiaaaaaaaaaa@users.noreply.github.com>
2024-07-20 16:56:07 +0300

    Update main_window.py

--- memory_card/main_window.py
+++ memory_card/main_window.py
@@ -1,138 +1,102 @@
-from main_window import *
-from menu_window import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
 
-from random import choice, shuffle
-from time import sleep
- 
-wins = [main_win, menu_win]
+main_win = QWidget()
+main_win.resize(600, 500)
+main_win.move(300, 300)
+main_win.setWindowTitle("Питання")
 
-for win in wins:
-    win.setStyleSheet('''
-                      color:yellow
-                      backround-color:lightblue
-                      font-size: 20xp;
-                      border: 0.5px solid blue:
-                      ''')
+#  
+# Створюємо потрібні віджети (Кнопки - таймер - надпис)
+# ----------------------------------------------------------
+btn_Menu = QPushButton('Меню')               # кнопка повернення до основного вікна
+btn_Sleep = QPushButton('Відпочити')         # кнопка прибирає вікно та повертає його після закінчення таймера
+btn_OK = QPushButton('Відповісти')           # кнопка відповіді
+box_Minutes = QSpinBox()                     # введення кількості хвилин
+box_Minutes.setValue(3)
+lb_Question = QLabel('')                     # текст питання
 
-lb_Result.setStyleSheet('margin: 20px;')
+# ----------------------------------------------------------
+#  Створюємо панель  - із варіантами відповідей - групуємо
+# ----------------------------------------------------------
+RadioGroupBox = QGroupBox("Варіанти відповідей:")    # Створення панелі-рамки для блоку 
+RadioGroup = QButtonGroup()                         # Організація в групу віджетів (кнопок)
 
-app = QApplication([])
+rbtn_1 = QRadioButton('')
+rbtn_2 = QRadioButton('')
+rbtn_3 = QRadioButton('')
+rbtn_4 = QRadioButton('')
 
-class Question():
-    def __init__(self,question,answer,wrong_answer1,wrong_answer2,wrong_answer3): # конструктор
-        self.question = question # питання
-        self.answer = answer # відповідь
-        self.wrong_answer1 = wrong_answer1 # неправильні відповіді 1 - 3
-        self.wrong_answer2 = wrong_answer2
-        self.wrong_answer3 = wrong_answer3
-        
-        self.actual = True # чи актуальне питання
-        self.attempts = 0 # спроби
-        self.correct = 0 # правильно чи ні
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
 
-    def got_right(self):
-        # змінює статистику, отримавши правильну відповідь
-        self.attempts += 1
-        self.correct += 1
-    
-    def got_wrong(self):
-        # змінює статистику, отримавши неправильну відповідь
-        self.attempts += 1
+                                    #Розміщуємо варіанти відповідей у стовпці всередині групи
+layout_ans1 = QHBoxLayout()
+layout_ans2 = QVBoxLayout()     # вертикальні будуть усередині горизонтального
+layout_ans3 = QVBoxLayout()
 
-# питання
-q1 = Question('Які зорі мають найвищу температуру', 'сині', 'жовті', 'білі', 'червоні')
-q2 = Question('Які зорі мають найнижчу температуру поверхні', 'білі', 'чорні', 'блакитні', 'червоні')
-q3 = Question('Які холодні зорі мають колір', 'червоні', 'білі', "жовті", "чорні")
-q4 = Question('Який місяць має тільки 28 днів', 'Лютий', 'Березень', 'Квітень', 'всі мають')
-q5 = Question('Скільки в Ураїні були президенти', '6', '8', '7', '5')
-q6 = Question('Перший президент України', 'Леонід Кравчук', 'Володимер Зеленський', 'Віктор Ющенко', 'Віктор Янукович')
-q7 = Question('Найвища гора?', 'Еверест', 'Говерла', 'Лхоцзе', 'Чогорі')
+layout_ans2.addWidget(rbtn_1)   #Дві відповіді в перший стовпець
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3)   #Дві відповіді у другий стовпець
+layout_ans3.addWidget(rbtn_4)
 
+                                        #Тепер перемикачі прив'язані до однієї горизонтальної направляючої лінії
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3)
+RadioGroupBox.setLayout(layout_ans1)
 
-# списки з кнопками та питаннями
-radio_list = [rbtn1, rbtn2, rbtn3, rbtn4] # кнопки
-questions = [q1, q2, q3, q4, q5, q6, q6, q7] # питання
-# задаємо питання
+# ----------------------------------------------------------
+# Створюємо панель із результатом тесту:
+# ----------------------------------------------------------
+AnsGroupBox = QGroupBox("Результат тесту:")
+lb_Result = QLabel('')                          # Напис "правильно"/"неправильно"
+lb_Correct = QLabel('')                         # Текст правильної відповіді
 
-def new_question():
-    global current_question
-    current_question = choice(questions) # питання
+                                # Розміщуємо 
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
+AnsGroupBox.setLayout(layout_res)
+AnsGroupBox.hide()                    # Можна приховати віджет.
 
-    lb_Question.setText(current_question.question) # беремо питання та правильну відповідь
-    lb_Correct.setText(current_question.answer)
+# ----------------------------------------------------------
+# Розміщуємо всі віджети у вікні:
+# ----------------------------------------------------------
+layout_line1 = QHBoxLayout()
+layout_line2 = QHBoxLayout()
+layout_line3 = QHBoxLayout()
+layout_line4 = QHBoxLayout()
 
-    shuffle(radio_list) # перемішуємо кнопки
+# розмінюємо на 1 шій лінії (кнопки меню,сну,і надпис)
+layout_line1.addWidget(btn_Menu)
+layout_line1.addStretch(1)
+layout_line1.addWidget(btn_Sleep)
+layout_line1.addWidget(box_Minutes)
+layout_line1.addWidget(QLabel("хвилин"))
 
-    radio_list[0].setText(current_question.wrong_answer1) # задаємо кнопкам варіанти відповідей
-    radio_list[1].setText(current_question.wrong_answer2)
-    radio_list[2].setText(current_question.wrong_answer3)
-    radio_list[3].setText(current_question.answer)
-new_question()
-# перевірка відповіді
-def check_result():
-    for ans_btn in radio_list: # отримуємо кнопки з списка
-        if ans_btn.isChecked(): # якщо кнопку нажали
-            if ans_btn.text() == lb_Correct.text(): # перевіряємо якщо текст кнопки правильний
-                current_question.got_right()
-                lb_Result.setText('Правильно') # правильно
-                break
-    else:
-        current_question.got_wrong()
-        lb_Result.setText('Неправильно') # неправильно
-# відпочити
-def rest(): 
-    main_wind.hide() # прячемо головне вікно
-    n = box_Minutes.value() # беремо встановлене число
-    sleep(n) # відпочинок
-    main_wind.show() # показуємо головне вікно
+# розмінюємо на 2 шій лінії надпис - питання
+layout_line2.addWidget(lb_Question,alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
 
-def show_menu(): # показати меню
-    menu_wind.show() # показуємо меню
-    main_wind.hide() # ховаємо головне вікно
+# розмінюємо на 3  тій лінії Рамки
+layout_line3.addWidget(RadioGroupBox)
+layout_line3.addWidget(AnsGroupBox)
 
-def back_menu(): # показати головне вікно
-    menu_wind.hide() # прячемо меню
-    main_wind.show() # показуємо головне вікно
+# розмінюємо на 4 тій лінії кнопку відповісти
+layout_line4.addStretch(1)
+layout_line4.addWidget(btn_OK,stretch=2)
+layout_line4.addStretch(1)
 
-def clear(): 
-    # очищуємо питання
-    txt_Question.clear()
-    txt_Answer.clear()
-    txt_Wrong1.clear()
-    txt_Wrong2.clear()
-    txt_Wrong3.clear()
+# 4 горизонтальні на 1 вертикальну
+layout_cards = QVBoxLayout()
+layout_cards.addLayout(layout_line1,stretch=1)
+layout_cards.addLayout(layout_line2,stretch=2)
+layout_cards.addLayout(layout_line3,stretch=8)
 
-def addQuestion(): # додаємо питання
-    newq = Question(txt_Question.text(), txt_Answer.text(), txt_Wrong1.text(), txt_Wrong2.text(), txt_Wrong3.text()) # задаємо варіанти відповіді
-    questions.append(newq) # додаємо питання в список
-    clear() # очищуємо
+layout_cards.addStretch(1)
+layout_cards.addLayout(layout_line4,stretch=1)
+layout_cards.addStretch(1)
+layout_cards.setSpacing(5) # прогалини між вмістом
 
-def switch_screen():
-    if btn_OK.text() == 'Відповісти':
-        check_result()
-        RadioGroupBox.hide()
-        AnsGroupBox.show()
-        btn_OK.setText('Наступне питання')
-    else:
-        new_question()
-        RadioGroupBox.show()
-        AnsGroupBox.hide()
-        btn_OK.setText('Відповісти')
-
-        RadioGroup.setExclusive(False)
-        rbtn1.setChecked(False)
-        rbtn2.setChecked(False)
-        rbtn3.setChecked(False)
-        rbtn4.setChecked(False)
-
-# Підключення виклику функції до кнопок
-
-btn_Menu.clicked.connect(show_menu)
-btn_back.clicked.connect(back_menu)
-btn_clear.clicked.connect(clear)
-btn_Sleep.clicked.connect(rest)
-btn_OK.clicked.connect(switch_screen)
-btn_add_q.clicked.connect(addQuestion)
-
-main_wind.show()
-app.exec_()
+main_win.setLayout(layout_cards) # передаємо на головне вікно основний макет
