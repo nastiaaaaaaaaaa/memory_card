c71def863fd702a7af62627527a00add95b3fdbc
nastiaaaaaaaaaa <116444052+nastiaaaaaaaaaa@users.noreply.github.com>
2024-07-20 16:53:27 +0300

    Update menu_window.py

--- memory_card/menu_window.py
+++ memory_card/menu_window.py
@@ -1,12 +1,13 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import * 
+from PyQt5.QtCore import Qt      
+from PyQt5.QtWidgets import *  
 
 app = QApplication([])
 
 menu_win = QWidget()
 menu_win.resize(400, 300)
+menu_win.setWindowTitle("Меню")
 
-txt_Question = QLineEdit('')        #Створює текстові поля
+txt_Question = QLineEdit('')
 txt_Answer = QLineEdit('')
 txt_Wrong1 = QLineEdit('')
 txt_Wrong2 = QLineEdit('')
@@ -14,23 +15,33 @@ txt_Wrong3 = QLineEdit('')
 
 layout_form = QFormLayout()
 
-layout_form.addRow('Питання', txt_Question)
-layout_form.addRow('Правильна відповідь:', txt_Answer)      #Ставить в правильному порядку текст і текстове поле
+layout_form.addRow('Питання:', txt_Question)
+layout_form.addRow('Правильна відповідь:', txt_Answer)
 layout_form.addRow('Неправильний варіант №1:', txt_Wrong1)
 layout_form.addRow('Неправильний варіант №2', txt_Wrong2)
-layout_form.addRow('Неправильний варіант №3', txt_Wrong3)
+layout_form.addRow('Неправильний варіант №3:', txt_Wrong3)
 
-btn_back = QPushButton('Назад')             #Додає кнопки повертання назад, додавання питання, очищення
-btn_add_q = QPushButton('Додати питання')   
+btn_back = QPushButton('Назад')
+btn_add_q = QPushButton('Додати питання')
 btn_clear = QPushButton('Очистити')
 
-hbtn = QHBoxLayout()        #Вирівнює кнопки по горизонталі
+# Add statistics label
+lbl_statistics = QLabel('')
+
+hbtn = QHBoxLayout()         
 hbtn.addWidget(btn_back)
 hbtn.addWidget(btn_add_q)
 hbtn.addWidget(btn_clear)
 
-vline = QVBoxLayout()           #Вирівнює все по вертикалі
+# Статистика
+lbl_statistics = QLabel('Статистика:\nПравильних відповідей: 0\nЗагальна кількість спроб: 0') # надпис статистики
+
+# Макет для основного вікна
+vline = QVBoxLayout()
 vline.addLayout(layout_form)
 vline.addLayout(hbtn)
+vline.addWidget(lbl_statistics) # додаємо його на макет
+
+menu_win.setLayout(vline)
 
 menu_win.setLayout(vline)
